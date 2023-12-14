d85f9dd06a75d20f5aa1046baa4e2d56282e92fa
hericlysdlarii <hericlysdlarii@gmail.com>
2023-12-14 11:24:04 -0300

    CSS global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { Task } from './Task';
+import { Header } from './components/Header';
+
+import './global.css'
 
 export function App() {
   return (
     <div>
-      <Task />
+      <Header />
+      <Task 
+        content="Terminar o desafio do Ignite"
+      />
+      <Task 
+        content="Não ficar louco"
+      />
     </div>
   )
 }
--- src/Task.tsx
+++ src/Task.tsx
@@ -1,4 +1,8 @@
-export function Task() {
-  return <p>Task</p>
+interface Props {
+  content: string;
+}
+
+export function Task(props:Props) {
+  return <p>{props.content}</p>
 }
 
