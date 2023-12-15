f7c3442a25f691b5bdad152bfb96f85903f85400
hericlysdlarii <hericlysdlarii@gmail.com>
2023-12-15 11:18:14 -0300

    Header da aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
 
+import todoLogo from '../assets/todo-logo.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>To-Do</strong>
+      <img src={todoLogo} alt="Logo do To-Do" />
+
     </header>
     
   );
